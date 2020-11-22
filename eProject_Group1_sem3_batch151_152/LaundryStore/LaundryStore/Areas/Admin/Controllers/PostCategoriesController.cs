be16bdf7f6a9c21d5da417505130d512fb922be7
zdhai1 <s2diamen@gmail.com>
2020-11-22 19:25:04 +0700

    hai commit

---
+++ eProject_Group1_sem3_batch151_152/LaundryStore/LaundryStore/Areas/Admin/Controllers/PostCategoriesController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LaundryStore.Models;
+
+namespace LaundryStore.Areas.Admin.Controllers
+{
+    public class PostCategoriesController : Controller
+    {
+        private LAUNDRY_PROJECTEntities db = new LAUNDRY_PROJECTEntities();
+
+        // GET: Admin/PostCategories
+        public ActionResult Index()
+        {
+            List<PostCategory> list = null;
+            list = db.PostCategories.Where(e => e.status == true).ToList();
+            return View(list);
+            
+        }
+
+        // GET: Admin/PostCategories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PostCategory postCategory = db.PostCategories.Find(id);
+            if (postCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(postCategory);
+        }
+
+        // GET: Admin/PostCategories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/PostCategories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,categoryNamePost,categoryCodePost,description,createdDate,createdBy,modifyDate,modifyBy,status")] PostCategory postCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PostCategories.Add(postCategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(postCategory);
+        }
+
+        // GET: Admin/PostCategories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PostCategory postCategory = db.PostCategories.Find(id);
+            if (postCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(postCategory);
+        }
+
+        // POST: Admin/PostCategories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit([Bind(Include = "id,categoryNamePost,categoryCodePost,description,createdDate,createdBy,modifyDate,modifyBy,status")] PostCategory postCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(postCategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(postCategory);
+        }
+
+        // GET: Admin/PostCategories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PostCategory postCategory = db.PostCategories.Find(id);
+            if (postCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(postCategory);
+        }
+
+        // POST: Admin/PostCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PostCategory postCategory = db.PostCategories.Find(id);
+            db.PostCategories.Remove(postCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
